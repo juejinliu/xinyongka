ad185622d8b1c4f05f6cc8266af03929b789c16a
kaixinjiuhao6654 <1157983684@qq.com>
2015-12-21 18:17:26 +0800

    huankuan
    huankuan update

--- operation.py
+++ operation.py
@@ -15,8 +15,39 @@ class Operation:
         for i in range(len(count_line)):
             return count_line
             # print(count_line[i])
+    def user_pay(self, user_name):
+        self.user_neme = user_name
+        get_file = open('user_consumption.txt', 'r+', encoding='utf8')
+        get_read = get_file.readlines()
+        count_line = []
+        for line in get_read:
+            if self.user_neme in line:
+                count_line.append(line.strip('\n'))
+        get_edu = count_line[len(count_line) - 1].split()
+        get_pay = int(get_edu[1]) - int(get_edu[2])
+        if get_pay == 0:
+            shuru = "没有要还的款"
+            return shuru
+        else:
+            print("你用还款的金额为%d" % get_pay)
+            user_input_pay = int(input("输入要还款的金额："))
+            while user_input_pay > get_pay:
+                print('输入的金额大于应还款金额，请重新输入！')
+                user_input_pay = int(input("输入要还款的金额："))
+            else:
+                get_edu[-2] = '-'+str(user_input_pay)
+                get_edu[2] = int(get_edu[2]) + user_input_pay
+                print(get_edu)
+                get_str = ' '.join(get_edu)
+                # print(type(get_edu))
+                print(get_str)
+                n = '\t'
+                new_user = n.join(str(get_edu))
+                print(new_user)
+                # get_file.write(new_user)
+                # get_file.close()
 
 if __name__ == '__main__':
     get = Operation()
-    get.get_user('王鹏飞')
-    print('a->还款')
+    get.user_pay('王鹏飞')
+    # print('a->还款')
--- xinyongka_index.py
+++ xinyongka_index.py
@@ -1,6 +1,4 @@
 from operation import Operation
-import os
-print(os.getcwd())
 
 user = input("请输入用户名：")
 passwd = input("请输入密码：")
@@ -14,20 +12,26 @@ for user in user_info:
 operation_dict = {'s':"查询", 'a':"添加", 'd':"删除"}
 for d, x in operation_dict.items():
     print(d+"->"+x)
-def judge(user_input):
+def judge():
     op_file = open("use_information.txt", 'r+', encoding='utf8')
     op_read = op_file.read()
-    # user_input = input("请输入用户名：")
-    while user_input not in op_read:
+    user_input = input("请输入用户名：")
+    while user_input not in op_read or user_input == '':
         print("输入的用户不存在，请重新输入！")
-        user_input = input("请输入用户名：")
+        user_input = input("请输入用户名：").strip()
+    return user_input
 a = input("请选择功能：")
 while a == 's':
-    user_input = input("输入用户名：")
-    judge(user_input)
+    # user_input = input("输入用户名：").strip()
+    user_input = judge()
     get = Operation()
     get_info = get.get_user(user_input)
     for i in range(len(get_info)):
         print(get_info[i])
     print('a->还款')
+    print('是否要还款，如果还款请输入a，退出请输入exit！')
+    user_input1 = input("请输入：")
+    if user_input1 == 'a':
+        get1 = Operation()
+        get1.user_pay(user_input)
 
